c613fc38478acf800ee5795aab0e544aa4ee34ac
fdiallo <fallaye.diallo@gmail.com>
2026-04-24 13:58:25 -0400

    Tested AlerBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-
-import './App.css'
 import AlertBox from './components/AlertBox/AlertBox'
 
 function App() {
 
   return (
     <>
-      <AlertBox type={'success'} message={'Message Received!'} />
-      <AlertBox type={'error'} message={'Undefined!' }/>
+      <AlertBox
+        type="success"
+        message="Your profile has been updated successfully!"
+        onClose={() => alert('Alert closed')}>
+        <p className="text-sm">You can now continue using the application.</p>
+      </AlertBox>
+
+      <AlertBox
+        type="error"
+        message="There has been an error updating your profile!"
+        onClose={() => alert('Alert closed')} >
+        <p className="text-sm">Please try again.</p>
+      </AlertBox>
     </>
   )
 }
